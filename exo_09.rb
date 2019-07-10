23d41562b9072ea507615d4a896b468d83f1edca
Alpha <ravalasonn@gmail.com>
2019-07-10 11:50:41 +0300

    Exercice 2.9

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Entrez votre nom de famille"
+print "> "
+first_name = gets.chomp
+
+puts "Entrez votre prénom"
+print "> "
+last_name = gets.chomp
+
+puts "Bonjour #{first_name + " " + last_name}"
