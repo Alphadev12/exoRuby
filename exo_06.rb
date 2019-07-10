e3dbc023fae4e287efec52471f9dbdd424561b51
Alpha <ravalasonn@gmail.com>
2019-07-10 11:24:31 +0300

    Exercice 2.6

---
+++ exo_06.rb
@@ -0,0 +1,9 @@
+number_of_hours_worked_a_day = 10
+number_of_day_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail: #{number_of_hours_worked_a_day * number_of_day_worked_per_week * number_of_weeks_in_THP}"
+
+puts "Et en minutes ça fait: #{number_of_minutes_in_an_hour * number_of_hours_worked_a_day * number_of_day_worked_per_week * number_of_weeks_in_THP}"
+
+#On a un erreur car number_of_minutes_in_an_hour n'est pas déclarer
