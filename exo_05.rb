56d4aafccf99c14f328ea6b3d00e5fd81f949216
Alpha <ravalasonn@gmail.com>
2019-07-10 11:13:14 +0300

    Exercice 2.5

---
+++ exo_05.rb
@@ -0,0 +1,24 @@
+puts "On va compter le nombre d'heures de travail à THP"    #affiche On va compter le nombre d'heures de travail à THP
+puts "Travail: #{10 * 5 * 11}" #affiche Trvail: et fait le calcul de 10 * 5 * 11
+puts "En minute ça fait: #{10 * 5 * 11 * 60}" #affiche En minute ça fait: et fait le calcul de 10 * 5 * 11 * 60
+
+puts "Et en seconde ?" #affiche Et en seconde?
+
+puts 10 * 5 * 11 * 60 * 60 #affiche le résultat du calcul de 10 * 5 * 11 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7?" #affiche Est-ce que c'est vrai que 3 + 2 < 5 - 7?
+
+puts  3 + 2 < 5 - 7 #Compare 3+2 et 5-7 et nous renvoie la valeur false car cette instruction est fausse
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"		#Affiche le résulat de 3 + 2
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"		#Affiche le résulat de 5 - 7
+
+puts "Ok, c'est faux alors !"		#Affiche Ok, c'est faux alors !
+
+puts "C'est drole ça, faisons-en plus :"		#C'est drole ça, faisons-en plus :
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"		#Test si 5 est supérieur à -2 et renvoie true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"		#Test si 5 est supérieur ou égal à -2 et renvoie true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"		#que 5 est inférieur ou égal à -2 et renvoie false
+
+# #{} nous premet de faire un calcul 
