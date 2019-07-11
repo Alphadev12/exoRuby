607a28e5d29e4b18bcc58f85c1b3f22749a5b037
Alpha <ravalasonn@gmail.com>
2019-07-11 08:05:52 +0300

    Exercice 2.18

---
+++ exo_18.rb
@@ -0,0 +1,22 @@
+puts "Entrer un email"
+print "> "
+
+email_generer = []
+
+email1 = "jean.dupont."
+email2 = "@email.fr"
+
+for count in (0..50) do
+
+	if count < 10
+		email_generer[count] = email1 + "0#{count}" + email2
+	else	
+		email_generer[count] = email1 + "#{count}" + email2
+	end
+	puts email_generer[count]
+end
+
+# (1..5).each do
+# 	email_generer = email
+# 	puts email_generer
+# end
